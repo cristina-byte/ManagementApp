33a220f56d547fbccf5827d48cc8a9b8c745ac23
cristina <cristinasiscanu30@gmail.com>
2023-06-13 21:50:54 +0300

    Delete comments from UsersController

--- ManagementTool/ManagementTool.API/Controllers/UsersController.cs
+++ ManagementTool/ManagementTool.API/Controllers/UsersController.cs
@@ -14,25 +14,6 @@ namespace ManagementTool.API.Controllers
     [Authorize]
     [Route("api/[controller]")]
     [ApiController]
-
-    //a url (from uniform resource locator) is a unique identifier used to
-    //locate a resource on the Internet.
-
-    //parts of a url 
-    //1 - scheme or protocol (http or https)
-    //2 - subdomain
-    //3 - domain name - actual name of the website. It must be unique. You can't register the same domain name twice. 
-    //Domain names are case insensitive. 
-    //4 - top-level domain (.com or .edu)
-    //5 - port
-    //6 - path
-    //7 - query - question mark tells the browser taht a query is being performed. 
-    //8 - parameters
-    //9 - fragments . It is preceded by a hash and directs to secondary resources.
-    //Often times the fragment will be an id of an id attribute of an HTML element. 
-
-    //https://managementTool.com:443/users?n=cristina&a=23#345
-
     public class UsersController : ControllerBase
     {
         private readonly IMapper _mapper;
